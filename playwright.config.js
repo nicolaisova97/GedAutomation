0837a820d4c874f94eb3da853971eecdda8375b9
nicolaisova97 <nicolai.sova97@gmail.com>
2025-02-08 15:00:54 +0200

    removing allure

--- playwright.config.js
+++ playwright.config.js
@@ -1,19 +1,18 @@
 // @ts-check
-const { devices, defineConfig } = require("@playwright/test");
+const { devices } = require("@playwright/test");
 
-const config = defineConfig({
+const config = {
   testDir: "./tests",
   retries: 0,
-  workers: 3, // how many test files to run in parallel
+  workers: 3, // how much test files to run in parralel
+  /* Maximum time one test can run for. */
+  //10-
   timeout: 30 * 1000,
   expect: {
     timeout: 5000,
   },
-  reporter: [
-    ["list"],
-    ["allure-playwright", { outputFolder: "allure-results" }],
-    ["html"],
-  ],
+
+  reporter: "html",
   projects: [
     {
       name: "chrome",
@@ -22,13 +21,15 @@ const config = defineConfig({
         headless: true,
         screenshot: "on",
         video: "retain-on-failure",
-        ignoreHttpsErrors: true,
-        permissions: ["geolocation"],
+        ignoreHttpsErrors: true, // ignore https security page
+        permissions: ["geolocation"], // allow permission to use geolocation from the left top corner
         trace: "on",
+        // ...devices['Pixel 5']
         viewport: { width: 1440, height: 720 },
       },
     },
   ],
-});
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+};
 
 module.exports = config;
