f602dd26a012f76ee1123365399ecfe65aaf6df1
nicolaisova97 <nicolai.sova97@gmail.com>
2025-02-11 14:13:56 +0200

    Adding some info

--- tests/SmokeTesting/GedPage.spec.js
+++ tests/SmokeTesting/GedPage.spec.js
@@ -1,64 +1,69 @@
-const { test, expect } = require('@playwright/test');
-const { POManager } = require('../../pageobjects/POManager');
+const { test, expect } = require("@playwright/test");
+const { POManager } = require("../../pageobjects/POManager");
 
 let poManager, loginPage, homePage, gedPage, searchPage;
 
 test.beforeEach(async ({ page }) => {
-    poManager = new POManager(page);
-    loginPage = poManager.getLoginPage();
-    homePage = poManager.getHomePage();
-    gedPage = poManager.getGedPage();
-    searchPage = poManager.getSearchPage();
+  poManager = new POManager(page);
+  loginPage = poManager.getLoginPage();
+  homePage = poManager.getHomePage();
+  gedPage = poManager.getGedPage();
+  searchPage = poManager.getSearchPage();
 });
 
-async function login()
-{
-    await loginPage.goTo();
-    await loginPage.validLogIn("test.md", "test.QA2024");
+async function login() {
+  await loginPage.goTo();
+  await loginPage.validLogIn("test.md", "test.QA2024");
 }
 
-async function loginAndNavigateToGed()
-{
-    await login();
-    await homePage.clickOnGed();
+async function loginAndNavigateToGed() {
+  await login();
+  await homePage.clickOnGed();
 }
 
-test('@Smoke: Open Ged page', async({page}) => {
-    await loginAndNavigateToGed();
-    await gedPage.verifyPageLink();
+test("@Smoke: Open Ged page", async ({ page }) => {
+  await loginAndNavigateToGed();
+  await gedPage.verifyPageLink();
 });
 
-test('@Smoke: Verify Dossiers', async({page}) => {
-    await loginAndNavigateToGed();
-    await page.waitForLoadState('networkidle');
-    await gedPage.verifyDossier();
+test("@Smoke: Verify Dossiers", async ({ page }) => {
+  await loginAndNavigateToGed();
+  await page.waitForLoadState("networkidle");
+  await gedPage.verifyDossier();
 });
 
-test('@Smoke: Verify Bannettes', async({page}) => {
-    await loginAndNavigateToGed();
-    await gedPage.verifyBannettes();
+test("@Smoke: Verify Bannettes", async ({ page }) => {
+  await loginAndNavigateToGed();
+  await gedPage.verifyBannettes();
 });
 
-test('@Smoke: Verify WorkFlow', async({page}) => {
-    await loginAndNavigateToGed();
-    await gedPage.verifyWorkflow();
+test("@Smoke: Verify WorkFlow", async ({ page }) => {
+  await loginAndNavigateToGed();
+  await gedPage.verifyWorkflow();
 });
 
-test('@Smoke: Drag and Drop a document to Document a classer on GED page', async({page}) => {
-    await loginAndNavigateToGed();
-    await page.setInputFiles('input[type="file"]', [
-        '/home/nick/Downloads/testAutomation.pdf'
-    ]);
-    await page.reload();
-    const documentLocator = page.locator("//a[contains(text(), 'testAutomation.pdf')]");
-    await expect(documentLocator).toBeVisible();
+test("@Smoke: Drag and Drop a document to Document a classer on GED page", async ({
+  page,
+}) => {
+  await loginAndNavigateToGed();
+  await page.setInputFiles('input[type="file"]', [
+    "/home/nick/Downloads/testAutomation.pdf",
+  ]);
+  await page.reload();
+  const documentLocator = page.locator(
+    "//a[contains(text(), 'testAutomation.pdf')]"
+  );
+  await expect(documentLocator).toBeVisible();
 });
 
-test('@Smoke: Standard search of a document by at least one keyword', async({page}) => {
-    await loginAndNavigateToGed();
-    await searchPage.searchForADocument("testAutomation");
-    const documentName = page.locator("//span[contains(text(), 'testAutomation')]");
-    await expect(documentName).toBeVisible();
-    await page.hover("#view-document-thumb-26");
+test("@Smoke: Standard search of a document by at least one keyword", async ({
+  page,
+}) => {
+  await loginAndNavigateToGed();
+  await searchPage.searchForADocument("testAutomation");
+  const documentName = page.locator(
+    "//span[contains(text(), 'testAutomation')]"
+  );
+  await expect(documentName).toBeVisible();
+  await page.hover("#view-document-thumb-26");
 });
-
--- tests/SmokeTesting/TableauDeBordPage.spec.js
+++ tests/SmokeTesting/TableauDeBordPage.spec.js
@@ -1,48 +1,46 @@
-const { test, expect } = require('@playwright/test');
-const { POManager } = require('../../pageobjects/POManager');
+const { test, expect } = require("@playwright/test");
+const { POManager } = require("../../pageobjects/POManager");
 
 let poManager, loginPage, tableauDeBordPage, searchPage, helpPage;
 
 test.beforeEach(async ({ page }) => {
-    poManager = new POManager(page);
-    loginPage = poManager.getLoginPage();
-    tableauDeBordPage = poManager.getTableauDeBordPage();
-    searchPage = poManager.getSearchPage();
-    helpPage = poManager.getHelpPage();
+  poManager = new POManager(page);
+  loginPage = poManager.getLoginPage();
+  tableauDeBordPage = poManager.getTableauDeBordPage();
+  searchPage = poManager.getSearchPage();
+  helpPage = poManager.getHelpPage();
 });
 
-async function login()
-{
-    await loginPage.goTo();
-    await loginPage.validLogIn("test.md", "test.QA2024");
+async function login() {
+  await loginPage.goTo();
+  await loginPage.validLogIn("test.md", "test.QA2024");
 }
 
-test('@Smoke: Open Tableau du Bord page', async() => {
-    await login();    
-    await tableauDeBordPage.verifyPageTitle();
+test("@Smoke: Open Tableau du Bord page", async () => {
+  await login();
+  await tableauDeBordPage.verifyPageTitle();
 });
 
-test('@Smoke: Search with empty data', async ({page}) => {
-    await login();
-    await searchPage.clickOnSearchInput();    
-    await searchPage.clickOnRechercherButton();
-    const errorText = await searchPage.getEmptySearchErrorText();
-    expect(errorText).toContain('Veuillez saisir votre recherche');
+test("@Smoke: Search with empty data", async ({ page }) => {
+  await login();
+  await searchPage.clickOnSearchInput();
+  await searchPage.clickOnRechercherButton();
+  const errorText = await searchPage.getEmptySearchErrorText();
+  expect(errorText).toContain("Veuillez saisir votre recherche");
 });
 
-test('@Smoke: Valid search', async ({page}) => {
-    await login();
-    await searchPage.clickOnSearchInput();  
-    await searchPage.typeSearchData('test');  
-    await searchPage.clickOnRechercherButton();
-    await searchPage.verifyTableHasRows();
+test("@Smoke: Valid search", async ({ page }) => {
+  await login();
+  await searchPage.clickOnSearchInput();
+  await searchPage.typeSearchData("test");
+  await searchPage.clickOnRechercherButton();
+  await searchPage.verifyTableHasRows();
 });
 
-test('@Smoke: Invalid search', async ({page}) => {
-    await login();
-    await searchPage.clickOnSearchInput();  
-    await searchPage.typeSearchData('kjdjgasidguasjkdas');  
-    await searchPage.clickOnRechercherButton();
-    await searchPage.verifyTableHasNoRows();
+test("@Smoke1: Invalid search", async ({ page }) => {
+  await login();
+  await searchPage.clickOnSearchInput();
+  await searchPage.typeSearchData("kjdjgasidguasjkdas");
+  await searchPage.clickOnRechercherButton();
+  await searchPage.verifyTableHasNoRows();
 });
-
